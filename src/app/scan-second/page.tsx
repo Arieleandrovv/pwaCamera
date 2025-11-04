0f700092ab33e0e3b1b4d6af4da99d6e1e586e6a
Arieleandrovv <Arieleandrovv@outlook.com>
2025-11-04 17:58:52 -0400

    add file reader

--- src/app/scan-second/page.tsx
+++ src/app/scan-second/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Html5Qrcode } from "html5-qrcode";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
 
 export default function ScanPage() {
   const scannerRef = useRef<Html5Qrcode | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [error, setError] = useState("");
+  const [result, setResult] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     const html5QrCode = new Html5Qrcode("reader");
@@ -14,15 +21,16 @@ export default function ScanPage() {
       try {
         await html5QrCode.start(
           { facingMode: "environment" },
-          { fps: 10, qrbox: 250 },
+          { fps: 10, qrbox: { width: 250, height: 250 } },
           (decodedText) => {
-            console.log("QR detectado:", decodedText);
+            setResult(decodedText);
             html5QrCode.stop();
           },
           (err) => console.warn(err)
         );
       } catch (err) {
-        console.error("No se pudo acceder a la cámara:", err);
+        console.error(err);
+        setError("No se pudo acceder a la cámara");
       }
     };
 
@@ -33,9 +41,64 @@ export default function ScanPage() {
     };
   }, []);
 
+  const handleSelectImage = () => fileInputRef.current?.click();
+
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    try {
+      const text = await scannerRef.current!.scanFile(file, true);
+      setResult(text);
+    } catch (err) {
+      console.error(err);
+      setError("No se pudo leer el QR desde la imagen.");
+    }
+  };
+
   return (
-    <div className="bg-black">
-      <div id="reader" className="w-full h-screen" />
+    <div className="relative flex flex-col items-center justify-center h-screen bg-black overflow-hidden w-full">
+      <div className="absolute top-6 z-10 text-center text-white">
+        <h1 className="text-2xl font-bold">Escanea el código QR</h1>
+        <p className="text-sm opacity-80">
+          para poder ver los ítems del pedido
+        </p>
+      </div>
+
+      <div id="reader" className="absolute inset-0 w-full z-0" />
+      <div className="absolute bottom-10 flex flex-col items-center z-10 w-full px-4">
+        {result && (
+          <p className="text-green-400 text-sm mb-2 text-center">
+            QR detectado: {result}
+          </p>
+        )}
+        {error && (
+          <p className="text-red-400 text-sm mb-2 text-center">{error}</p>
+        )}
+
+        <Input
+          ref={fileInputRef}
+          type="file"
+          accept="image/*"
+          onChange={handleFileChange}
+          className="hidden"
+        />
+
+        <Button
+          onClick={handleSelectImage}
+          className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg shadow-md"
+        >
+          Abrir galería
+        </Button>
+        <Button
+          onClick={() => router.push("/")}
+          className="bg-red-600 hover:bg-red-400 text-white px-4 py-2 rounded-lg shadow-md mt-4"
+        >
+          regresar
+        </Button>
+      </div>
     </div>
   );
 }
