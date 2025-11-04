cff207a09adf0504f58c01a4df6e44f37d2171a6
Arieleandrovv <Arieleandrovv@outlook.com>
2025-11-04 17:09:00 -0400

    minimalista

--- src/app/scan-second/page.tsx
+++ src/app/scan-second/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Html5Qrcode } from "html5-qrcode";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useRouter } from "next/navigation";
 
 export default function ScanPage() {
   const scannerRef = useRef<Html5Qrcode | null>(null);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [error, setError] = useState("");
-  const [result, setResult] = useState("");
-  const router = useRouter();
 
   useEffect(() => {
     const html5QrCode = new Html5Qrcode("reader");
@@ -23,14 +16,13 @@ export default function ScanPage() {
           { facingMode: "environment" },
           { fps: 10, qrbox: 250 },
           (decodedText) => {
-            setResult(decodedText);
+            console.log("QR detectado:", decodedText);
             html5QrCode.stop();
           },
           (err) => console.warn(err)
         );
       } catch (err) {
-        console.error(err);
-        setError("No se pudo acceder a la cámara");
+        console.error("No se pudo acceder a la cámara:", err);
       }
     };
 
@@ -41,27 +33,9 @@ export default function ScanPage() {
     };
   }, []);
 
-  const handleSelectImage = () => fileInputRef.current?.click();
-
-  const handleFileChange = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const file = event.target.files?.[0];
-    if (!file) return;
-
-    try {
-      const text = await scannerRef.current!.scanFile(file, true);
-      setResult(text);
-    } catch (err) {
-      console.error(err);
-      setError("No se pudo leer el QR desde la imagen.");
-    }
-  };
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen overflow-hidden w-full">
-      <div id="reader" className="inset-0 w-full h-screen z-0" />
-      {/*<div className="absolute w-64 h-64 border-4 border-white rounded-lg z-10" />*/}
+    <div className="w-screen h-screen overflow-hidden bg-black">
+      <div id="reader" className="w-full h-full" />
     </div>
   );
 }
